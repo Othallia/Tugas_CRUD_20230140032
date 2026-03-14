b4f46c19ccd07ce9d7169f25abeeeef3e193e774
Othallia <vianiputriocta6@gmail.com>
2026-03-14 14:21:20 +0700

    Commit 3: Membuat Entity KTP

---
+++ src/main/java/com/deploy/tugascrud_ktp/model/entity/Ktp.java
@@ -0,0 +1,33 @@
+package com.deploy.tugascrud_ktp.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "KTP") 
+public class Ktp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String nomorKtp;
+
+    @Column(nullable = false)
+    private String namaLengkap;
+
+    private String alamat;
+
+    private LocalDate tanggalLahir;
+
+    private String jenisKelamin;
+}

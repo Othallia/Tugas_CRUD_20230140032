e2ee6022d677cd96632d889f003a8010404be3ba
Othallia <vianiputriocta6@gmail.com>
2026-03-14 14:07:14 +0700

    Commit 1: Inisialisasi project dan update pom.xml

---
+++ src/main/java/com/deploy/tugascrud_ktp/TugascrudKtpApplication.java
@@ -0,0 +1,13 @@
+package com.deploy.tugascrud_ktp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugascrudKtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugascrudKtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deploy/tugascrud_ktp/TugascrudKtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.tugascrud_ktp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugascrudKtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
